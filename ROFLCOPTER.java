91180ae105f3d9d3f55ed3de364b0635377631a4
Tia-beanie <144304264+Tia-beanie@users.noreply.github.com>
2024-09-29 16:51:25 -0700

    Add files via upload

---
+++ ROFLCOPTER.java
@@ -0,0 +1,119 @@
+public class ROFLCOPTER{
+    public static void main(String[] args){
+
+        while(true){
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("ROFL:ROFL:LO                       ");
+            System.out.println("           A            ");
+            System.out.println("  L    /--------                 ");
+            System.out.println(" LOL====      []\\              ");
+            System.out.println("       \\         \\      ");
+            System.out.println("         \\________]              ");
+            System.out.println("            I   I       ");
+            System.out.println("         ---------/               ");
+            System.out.println("  Grace's ROFLCOPTER!                     ");
+            System.out.println("                       ");
+
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("           OL:ROFL:ROFL                       ");
+            System.out.println("           A            ");
+            System.out.println("       /--------                 ");
+            System.out.println(" LOL====      []\\              ");
+            System.out.println("  L    \\         \\      ");
+            System.out.println("         \\________]              ");
+            System.out.println("            I   I       ");
+            System.out.println("         ---------/               ");
+            System.out.println("  Grace's ROFLCOPTER!                     ");
+            System.out.println("                       ");
+
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("    ROFL:ROFL:O                       ");
+            System.out.println("              A            ");
+            System.out.println("          /--------                 ");
+            System.out.println("    LOL====      []\\              ");
+            System.out.println("     L    \\         \\      ");
+            System.out.println("            \\________]              ");
+            System.out.println("               I   I       ");
+            System.out.println("            ---------/               ");
+            System.out.println("      Grace's ROFLCOPTER!                     ");
+            System.out.println("                          ");
+
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+            System.out.println("              O:ROFL:ROFL                       ");
+            System.out.println("              A            ");
+            System.out.println("     L    /--------                 ");
+            System.out.println("    LOL====      []\\              ");
+            System.out.println("          \\         \\      ");
+            System.out.println("            \\________]              ");
+            System.out.println("               I   I       ");
+            System.out.println("            ---------/               ");
+            System.out.println("     Grace's ROFLCOPTER!                     ");
+            System.out.println("                       ");
+
+            try{
+                Thread.sleep(100);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
